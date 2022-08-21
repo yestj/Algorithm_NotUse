b8a09db4b6c0cf92e01e5814f9fab1e1baed00a1
yezi <yejixheo@gmail.com>
2022-08-21 14:54:07 +0900

    [SWEA/D3/1216]: solved

--- swea/src/d3/No_1216.java
+++ swea/src/d3/No_1216.java
@@ -11,22 +11,55 @@ public class No_1216 {
 		for (int t = 0; t < 10; t++) {
 			int tc = sc.nextInt();
 
-			char[][] arr = new char[100][];
-			for (int i = 0; i < 100; i++) {
-				String row = sc.next();
-				arr[i] = row.toCharArray();
-			}
-
-			int maxLen = 0;
-			// 가로 탐색.
+			// 2차원 배열에 문자 나누어 입력하기.
+			char[][] arr = new char[100][100];
 			for (int r = 0; r < 100; r++) {
+				String input = sc.next();
 				for (int c = 0; c < 100; c++) {
-					for (int len = 100; len>0; len--) {
-						
-					}
+					arr[r][c] = input.charAt(c);
 				}
 			}
+
+			int result = 0;
+			for(int i = 100; i > 0; i--) {
+				if(check(arr, i)) {
+					result = i;
+					break;
+				}
+			}
+			
+			
+			System.out.println("#" + tc + " " + result);
+
 		}
+	}
+	
+	static boolean check(char[][] arr, int len) {
+		
+		for(int i = 0; i < 100; i++) {
+			for (int j = 0; j <= 100-len; j++) {
+				if(checkRow(arr, i, j, len) || checkCol(arr, j, i, len)) return true;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	// 가로 검사.
+	static boolean checkRow(char[][] arr, int r, int c, int len) {
+		for(int i = 0; i < len/2; i++) {
+			if(arr[r][c+i] != arr[r][c+len-1-i]) return false;
+		}
+		return true;
+	}
+	
+	// 세로 검사.
+	static boolean checkCol(char[][] arr, int r, int c, int len) {
+		for(int i = 0; i < len/2; i++) {
+			if(arr[r+i][c] != arr[r+len-1-i][c]) return false;
+		}
+		return true;
 	}
 
 }
